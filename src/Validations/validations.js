7388825018dac5222ac457f670408934e3f155fa
Gaurav Kandhalia <vaidkandhala333@gmail.com>
2025-07-20 16:24:36 +0530

    utils completed

---
+++ src/Validations/validations.js
@@ -0,0 +1,12 @@
+import {z} from "zod";
+
+const { z } = require("zod");
+
+ export const signupSchema = z.object({
+  fullName: z.string().min(3, "Full name must be at least 3 characters"),
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+  role: z.enum(["employee", "admin"]).optional(), // default can be handled in Mongoose
+});
+
+
